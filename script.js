7c651aeb5804f7a78f783774c405d8a51d5e6295
aswathamaallu007-star <aswathamaallu786@gmail.com>
2025-09-10 12:50:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
     //Write your code here
+	const form = document.getElementById('form1')
+	let fname = form.elements['fname'].value
+	fname = fname.trim()
+	let lname = form.elements['lname'].value
+	lname = lname.trim()
+	alert(`${fname} ${lname}`)
 
 }
